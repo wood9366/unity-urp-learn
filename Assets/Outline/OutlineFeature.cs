b64b297f8651090dc6eb09deb6e904539957fc41
liyang <7325560@qq.com>
2022-04-27 23:20:02 +0800

    create outline draw shape rt with depth

--- Assets/Outline/OutlineFeature.cs
+++ Assets/Outline/OutlineFeature.cs
@@ -64,7 +64,7 @@ public class OutlineFeature : ScriptableRendererFeature
         {
             _desc = cameraTextureDescriptor;
 
-            var desc = new RenderTextureDescriptor(_desc.width, _desc.height, RenderTextureFormat.RG16, 0);
+            var desc = new RenderTextureDescriptor(_desc.width, _desc.height, RenderTextureFormat.RG16, 8);
 
             cmd.GetTemporaryRT(_rtShape, desc, FilterMode.Bilinear);
 
